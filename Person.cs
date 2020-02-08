d6d5b24b8d5fc997c4b927f0794d9686be4f9005
Samuel Janek <samueljanek17@gmail.com>
2020-02-08 12:09:12 +0100

    First version

---
+++ Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_filmManagmet
+{
+    /// <summary>
+    /// This class contains all informations about actors and directors
+    /// Actors and directors are separated into two different classes
+    /// Actors will be objects from class Actor, Directors will be objects from class Director
+    /// </summary>
+    class Person
+    {
+        class Actor
+        {
+            // Properties of the class
+            string firstName;
+            string secondName;
+            int age;
+            string[] moviesWithThisActor;
+            // TODO: Photography ????
+            // TODO: link to his profil on wiki ???
+
+            // Default constructor
+            public Actor()
+            {
+                firstName = null;           // TODO: use null or string.Empty -> if string.Empty than it need to be changed everywhere
+                secondName = null;
+                age = 0;
+                moviesWithThisActor = null;
+            }
+
+            // CONTINUE
+
+        }
+
+        class Director
+        {
+            // Properties of the class
+            string firstName;
+            string secondName;
+            int age;
+            string[] filmsHeDirected;
+            // TODO: Photography ????
+            // TODO: link to his profil on wiki ???
+
+            // Default constructor
+            public Director()
+            {
+                firstName = null;
+                secondName = null;
+                age = 0;
+                filmsHeDirected = null;
+            }
+
+            // CONTINUE
+
+        }
+    }
+}
---
+++ Ratings.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_filmManagmet
+{
+    /// <summary>
+    /// This class contains user's rating of one film.
+    /// For every new film, new object from this class need to be created
+    /// </summary>
+    class Ratings
+    {
+        // Properties of the class
+        int ratingInPercents;
+        StringBuilder textRating;
+
+        // Default constructor which sets the values to properties when new object is created
+        public Ratings()                // TODO: is constructor useless here ???
+        {
+            ratingInPercents = 0;
+            textRating = null;
+        }
+
+        // After clicking on "Add rating" button, new object "rating to film xx" is created and init. via constructor
+        // Than user will approach exact properties via getters and setters ( can also leave some properties empty )
+
+        #region Getters and Setters
+
+        // Methods for getting and setting the value of ratingInPercents
+        public int RatingInPercents
+        {
+            get { return ratingInPercents; }
+            set { ratingInPercents = value; }
+        }
+
+        // Methods for getting and setting the content of textRating
+        public StringBuilder TextRating
+        {
+            get { return textRating; }
+            set { textRating = value; }
+        }
+
+        #endregion
+
+    }
+}
